9b6c970f39fe52b864b9fe8bf39ecef2efe4894c
PerchunPak <git@perchun.it>
2023-05-12 20:34:19 +0200

    Add a bunch of new features and fixes
    - Handle 404 correctly.
    - Add UserAgent in case if someone will want to contact me for spam.
    - Add feature for saving all found files to local system.
    - Handle rate limit by Discord.
    - Add typing.
    - Sort embeds in update.
    - Automatically create required folders, if they don't exist.

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ Just find a place where you can run it periodically, and set these enviroment va
 - `USER_ID` - your Discord user ID to ping.
 - `SENTRY_DSN` - key for [Sentry](https://sentry.io). Needed to track the errors, because I do not
     look into logs anyway.
+- `DOWNLOAD_FILES` - set to anything (example `1`) if you want to download all the found files.
--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import hashlib
 import json
 import os
 import typing as t
+from zoneinfo import ZoneInfo
 
+import aiofiles
 import aiohttp
 import dictdiffer
 import sentry_sdk
 
 URL_WEBHOOK = os.environ["URL_WEBHOOK"]
 USER_ID = os.environ["USER_ID"]
+DOWNLOAD_FILES = bool(os.environ.get("DOWNLOAD_FILES"))
 
 
 # fmt: off
@@ -19,6 +22,20 @@ DIFF_TYPE: t.TypeAlias = \
   | tuple[t.Literal["change"], str,           tuple[str, str]       ] \
   | tuple[t.Literal["remove"], t.Literal[""], list [tuple[str, str]]]
 # fmt: on
+
+
+def get_current_time() -> datetime.datetime:
+    return datetime.datetime.now(tz=ZoneInfo("Europe/Prague"))
+
+
+def get_id_by_file_name(file_name: str) -> int:
+    return int(
+        file_name.removeprefix("2023_dalsi_kola_PR_skoly_")
+        .removeprefix("hmp-")
+        .removeprefix("soukrome-")
+        .removesuffix(".pdf")
+        .removesuffix(".xlsx")
+    )
 
 
 class RateLimiter:
@@ -82,18 +99,40 @@ class DataGetter:
             tasks.add(asyncio.create_task(self._send_request(name), name=name))
         return tasks
 
-    async def _send_request(self, name) -> bytes:
+    async def _send_request(self, name) -> bytes | t.Literal[False]:
         async with self._rate_limiter:
             async with self._session.get(
-                "https://www.prahaskolska.eu/wp-content/uploads/2023/05/" + name
+                "https://www.prahaskolska.eu/wp-content/uploads/2023/05/" + name,
+                headers={"User-Agent": "2nd-circle-updates"},
             ) as response:
-                if response.status not in {200, 404}:
-                    raise RuntimeError(f"Unknown status code: {response.status}")
+                if response.status == 404:
+                    return False
 
+                response.raise_for_status()
+                await save_to_file(response, name)
                 return await response.read()
 
-    def _handle_result(self, file: bytes, name: str) -> None:
+    def _handle_result(self, file: bytes | t.Literal[False], name: str) -> None:
+        if file is False:
+            return
+
         self._results[name] = hashlib.sha256(file).hexdigest()
+
+
+async def save_to_file(response: aiohttp.ClientResponse, name: str) -> None:
+    if not DOWNLOAD_FILES:
+        return
+
+    is_private = "soukrome" in name
+    is_excel = name.endswith(".xlsx")
+    number = get_id_by_file_name(name)
+
+    async with aiofiles.open(
+        f"data/saves/{'private' if is_private else 'free'}/{'excel' if is_excel else 'pdf'}/{number}.{'xlsx' if is_excel else 'pdf'}",
+        "wb",
+    ) as file:
+        async for chunk in response.content.iter_chunked(1024):
+            await file.write(chunk)
 
 
 async def report_to_discord(diff: DIFF_TYPE) -> None:
@@ -108,12 +147,14 @@ async def report_to_discord(diff: DIFF_TYPE) -> None:
             await _send_report(session, to_send)
 
             to_send = []
-        
+
         if len(to_send) > 0:
             await _send_report(session, to_send)
 
 
-async def _send_report(session: aiohttp.ClientSession, embeds: list[dict[str, str]]) -> None:
+async def _send_report(
+    session: aiohttp.ClientSession, embeds: list[dict[str, str]]
+) -> None:
     async with session.post(
         URL_WEBHOOK,
         json={
@@ -122,12 +163,22 @@ async def _send_report(session: aiohttp.ClientSession, embeds: list[dict[str, st
             "allowed_mentions": {"parse": ["roles", "users", "everyone"]},
         },
     ) as response:
-        if response.status != 200:
-            raise RuntimeError("Failed to report")
+        if response.status == 429:  # rate limited
+            to_sleep = (int((await response.json())["retry_after"]) / 1000) + 0.15
+            print(f"Discord rate limited me, waiting {to_sleep} seconds...")
+            await asyncio.sleep(to_sleep)
+            await _send_report(session, embeds)
+            return
+
+        response.raise_for_status()
 
 
 def _build_embed(
-    event_name: str, color: t.Literal["green"] | t.Literal["yellow"] | t.Literal["red"], file_name: str, file_hash: str | None, footer_text:str="File hash: {file_hash}"
+    event_name: str,
+    color: t.Literal["green"] | t.Literal["yellow"] | t.Literal["red"],
+    file_name: str,
+    file_hash: str | None,
+    footer_text: str = "File hash: {file_hash}",
 ):
     return {
         "title": "File was " + event_name,
@@ -135,6 +186,7 @@ def _build_embed(
         "url": "https://www.prahaskolska.eu/wp-content/uploads/2023/05/" + file_name,
         "color": {"green": 65280, "yellow": 16776960, "red": 16711680}[color],
         "footer": {"text": footer_text.format(file_hash=file_hash)},
+        "timestamp": get_current_time().isoformat(),
     }
 
 
@@ -145,7 +197,9 @@ def parse_embeds_to_report(diff: DIFF_TYPE) -> list[dict[str, str]]:
         if type == "add":
             for file_name, file_hash in value:
                 embeds.append(
-                    _build_embed("added", "green", file_name=file_name, file_hash=file_hash)
+                    _build_embed(
+                        "added", "green", file_name=file_name, file_hash=file_hash
+                    )
                 )
         elif type == "change":
             key, (old_value, new_value) = value
@@ -161,7 +215,9 @@ def parse_embeds_to_report(diff: DIFF_TYPE) -> list[dict[str, str]]:
         elif type == "remove":
             for file_name, file_hash in value:
                 embeds.append(
-                    _build_embed("removed", "red", file_name=file_name, file_hash=file_hash)
+                    _build_embed(
+                        "removed", "red", file_name=file_name, file_hash=file_hash
+                    )
                 )
         else:
             try:
@@ -169,17 +225,39 @@ def parse_embeds_to_report(diff: DIFF_TYPE) -> list[dict[str, str]]:
             except RuntimeError as error:
                 sentry_sdk.capture_exception(error)
 
+    embeds.sort(
+        key=lambda embed: get_id_by_file_name(embed["description"][1:-1])
+        + (1000 if embed["description"][1:-1].endswith(".xlsx") else 0)
+    )
     return embeds
+
+
+def prepare_folders() -> None:
+    if not os.path.exists("data"):
+        os.mkdir("data")
+    if not os.path.exists("data/latest.json"):
+        with open("data/latest.json", "w") as data_file:
+            json.dump({"date": get_current_time().isoformat()}, data_file)
+
+    if DOWNLOAD_FILES:
+        for folder_to_create in [
+            "data/saves/free/pdf",
+            "data/saves/free/excel",
+            "data/saves/private/pdf",
+            "data/saves/private/excel",
+        ]:
+            os.makedirs(folder_to_create, exist_ok=True)
 
 
 async def main() -> None:
     sentry_sdk.init(dsn=os.environ["SENTRY_DSN"], traces_sample_rate=1.0)
 
+    prepare_folders()
     with open("data/latest.json", "r") as data_file:
         previous_run = json.load(data_file)
 
     new_data = await DataGetter().get_data()
-    new_data["date"] = datetime.datetime.now().isoformat()
+    new_data["date"] = get_current_time().isoformat()
 
     os.rename(
         "data/latest.json",
